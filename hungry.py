1956257ff9dc81f8fce7b8e6553230cd2ab7b386
Aarushi <2041004131.arushisekhardeo@gmail.com>
2024-02-25 18:34:51 +0530

    fries

--- hungry.py
+++ hungry.py
@@ -2,7 +2,7 @@ hungry=input("are you hungry?")
 if hungry == "yes":
     print("Eat samosa")
     print("eat pizza")
-    print("eat burger")
+    print("eat fries")
 else:
     print("do your homework")
 
